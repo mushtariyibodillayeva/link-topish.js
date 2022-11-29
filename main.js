f07d650ccdf0301947abef62dec4138c73b16b62
mushtariyibodillayeva <dilfuzaxamrayeva53@gmail.com>
2022-11-29 16:26:36 +0500

    body-bg-color

---
+++ main.js
@@ -0,0 +1,16 @@
+// function next() {
+//   var x = document.baseURI;
+//   document.getElementById("net").innerHTML =
+//     "Bu siz foydalanayotgan sayt linki: " + x;
+// }
+// function next() {
+//   var net = window.open();
+//   net.document.open();
+//   net.document.write("<h1>Bajarildi</h1>");
+//   net.document.close();
+// }
+function next() {
+  document.open();
+  document.write("<h1>Yopildi</h1>");
+  document.close();
+}
